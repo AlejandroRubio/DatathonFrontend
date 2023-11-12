de7cfa17a9912f5eeb11f674aa951e5f6bb25949
Alejandro Rubio <alejandro.rubio.ingeniero@gmail.com>
2023-11-12 22:53:26 +0100

    Agregar archivos de proyecto.

---
+++ HudAsp/Controllers/TablesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HudAsp.Models;
+
+namespace HudAsp.Controllers;
+
+public class TablesController : Controller
+{
+	public IActionResult TableElements()
+	{
+		return View();
+	}
+
+	public IActionResult TablePlugins()
+	{
+		return View();
+	}
+
+	public IActionResult Demo()
+	{
+		return View();
+	}
+}
