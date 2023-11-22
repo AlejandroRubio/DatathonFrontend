92e40419cef1d727bc97ab95a2994c0d1bf40a0d
Alejandro Rubio <alejandro.rubio.ingeniero@gmail.com>
2023-11-22 22:05:15 +0100

    Incorporación nuevas tablas

--- HudAsp/Controllers/TablesController.cs
+++ HudAsp/Controllers/TablesController.cs
@@ -6,12 +6,36 @@ namespace HudAsp.Controllers;
 
 public class TablesController : Controller
 {
-	public IActionResult TableElements()
+	public IActionResult TableSalary()
 	{
 		return View();
 	}
+    public IActionResult TableSalaryPlus()
+    {
+        return View();
+    }
 
-	public IActionResult TablePlugins()
+    public IActionResult TableIndustry()
+    {
+        return View();
+    }
+    public IActionResult TableIndustryPlus()
+    {
+        return View();
+    }
+    public IActionResult TableHeatMap()
+    {
+        return View();
+    }
+
+    public IActionResult TableHeatMapPlus()
+    {
+        return View();
+    }
+
+
+
+    public IActionResult TablePlugins()
 	{
 		return View();
 	}
